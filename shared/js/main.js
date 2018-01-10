1b4738aa242069f1ccf9a727ace0b2bdc74fdc4b
vula <vula@bit-vietnam.com>
2018-01-10 17:47:21 +0700

    Doing progress bar

--- shared/js/main.js
+++ shared/js/main.js
@@ -1,8 +1,131 @@
 $('document').ready(function(){
   var body = $('body');
+  var qtimg, qttime, qtname, qtleft, qtright, pathImg, imgJpg;
+  qtimg = $('.qtimg');
+  qttime = $('.qttime');
+  qtname = $('.qtname');
+  qtleft = $('.qtleft');
+  qtright = $('.qtright');
+  pathImg = 'shared/img/question/';
+  imgJpg = '.jpg';
+  var success = 'shared/img/common/success.gif';
+  var failed = 'shared/img/common/failed.gif';
+  var gameStart = 'shared/img/common/start.gif';
 
-$( ".test" ).find( "div" ).eq( 0 ).addClass( "blue" );
+  var arr1 = [
+    {img: "testquestion1", name: "Ai đây1?", left: "Zuka1", right: "Wukong1", anleft: 0, anright: 1},
+    {img: "testquestion2", name: "Ai đây2?", left: "Zuka2", right: "Wukong2", anleft: 1, anright: 0},
+    {img: "testquestion3", name: "Ai đây3?", left: "Zuka3", right: "Wukong3", anleft: 1, anright: 0},
+    {img: "testquestion4", name: "Ai đây4?", left: "Zuka4", right: "Wukong4", anleft: 0, anright: 1},
+    {img: "testquestion5", name: "Ai đây5?", left: "Zuka5", right: "Wukong5", anleft: 0, anright: 1},
+    {img: "testquestion6", name: "Ai đây6?", left: "Zuka6", right: "Wukong6", anleft: 1, anright: 0},
+    {img: "testquestion7", name: "Ai đây7?", left: "Zuka7", right: "Wukong7", anleft: 1, anright: 0},
+    {img: "testquestion8", name: "Ai đây8?", left: "Zuka8", right: "Wukong8", anleft: 1, anright: 0},
+    {img: "testquestion9", name: "Ai đây9?", left: "Zuka9", right: "Wukong9", anleft: 0, anright: 1}
+  ]
 
+  randomGenerator.prototype = {
+    reset: function() {
+      this.remaining = [];
+      for (var i = this.low; i <= this.high; i++) {
+        this.remaining.push(i);
+      }
+    },
+    get: function() {
+      if (!this.remaining.length) {
+        this.reset();
+      }
+      var index = Math.floor(Math.random() * this.remaining.length);
+      var val = this.remaining[index];
+      this.remaining.splice(index, 1);
+      return val;        
+    }
+  }
+
+  var totalTime = 2;
+  var timeshow = progress(totalTime, totalTime, $('#progressBar'));  
+
+  var r = new randomGenerator(0, 8);
+  insertQuestion(arr1,r);
+
+  $('.qtresult > div').click(function() {
+    // clearTimeout(timeDelay);
+    // var answerSelect = $(this).children().attr('data-answer');
+    // if(answerSelect != 0){
+    //   $('.result-panel').addClass('active').children().attr('src', success);
+    //   var timeDelay = setTimeout(function() {
+    //     $('.result-panel').removeClass('active');
+    //     insertQuestion(arr1,r);
+    //   }, 1000);
+    // }else{
+    //   $('.result-panel').addClass('active').children().attr('src', failed);
+    //   var timeDelay = setTimeout(function() {
+    //     $('.result-panel').removeClass('active');
+    //     ShowSignUp('#restart', 'none');
+    //   }, 2000);
+    // }
+    console.log(timeshow);
+  });
+
+  $('.btn-start').click(function() {
+    // var counter = totalTime;
+    // var interval = setInterval(function() {
+    //   counter--;
+    //   if (counter < 1) {
+    //     clearInterval(interval);
+    //   }
+    // }, 1000);
+    ShowSignUp('#main');
+  });
+
+  $('.btn-restart').click(function() {
+    clearTimeout(timeDelay);
+    $('.result-panel').addClass('active').children().attr('src', gameStart);
+      var timeDelay = setTimeout(function() {
+        $('.result-panel').removeClass('active');
+        insertQuestion(arr1,r);
+        ShowSignUp('#main', 'none');
+      }, 2000);
+  });
+
+  function insertQuestion(newArr, r){
+    var elementArr = newArr[r.get()];
+    // console.log(elementArr.name);
+    qtimg.children().attr("src", pathImg + elementArr.img + imgJpg);
+    qtname.text(elementArr.name);
+    qtleft.children().text(elementArr.left).attr({'data-answer': elementArr.anleft});
+    qtright.children().text(elementArr.right).attr({'data-answer': elementArr.anright});
+  };
+
+  function ShowSignUp(page,effect){
+    if(effect !== null){
+      $.mobile.changePage( page, { transition: effect} );
+    }else{
+      $.mobile.changePage( page );
+    }
+  }
+
+  function randomGenerator(low, high) {
+    if (arguments.length < 2) {
+      high = low;
+      low = 0;
+    }
+    this.low = low;
+    this.high = high;
+    this.reset();
+  }
+
+  function progress(timeleft, timetotal, $element) {
+    var timeleft1 = timeleft;
+    var progressBarWidth = timeleft * $element.width() / timetotal;
+    $element.find('div').animate({ width: progressBarWidth }, timeleft == timetotal ? 0 : 1000, 'linear');
+    if(timeleft > 0) {
+      setTimeout(function() {
+        progress(timeleft - 1, timetotal, $element);
+      }, 1000);
+    }
+    return timeleft1;
+  }
 
 // var arr = ["mon","tues","wed"];
 // var arr = [];
@@ -26,53 +149,6 @@ $( ".test" ).find( "div" ).eq( 0 ).addClass( "blue" );
 // arr.testMe = function(){
 //   console.log('i am "testMe", a property of the array "arr", that happens to be an anonymour function');
 // };
-
-var arr1 = [
-  {name: "anhvu", phone: "123"},
-  {name: "anhhung", phone: "456"},
-  {name: "anhC", phone: "789"},
-  {name: "anhD", phone: "741"},
-  {name: "anhE", phone: "147"},
-  {name: "anhF", phone: "258"},
-  {name: "anhG", phone: "369"}
-]
-
-function randomGenerator(low, high) {
-  if (arguments.length < 2) {
-    high = low;
-    low = 0;
-  }
-  this.low = low;
-  this.high = high;
-  this.reset();
-}
-
-randomGenerator.prototype = {
-  reset: function() {
-    this.remaining = [];
-    for (var i = this.low; i <= this.high; i++) {
-      this.remaining.push(i);
-    }
-  },
-  get: function() {
-    if (!this.remaining.length) {
-      this.reset();
-    }
-    var index = Math.floor(Math.random() * this.remaining.length);
-    var val = this.remaining[index];
-    this.remaining.splice(index, 1);
-    return val;        
-  }
-}
-
-var r = new randomGenerator(0, 6);
-console.dir(arr1);
-
-$('.clickMe').click(function(event) {
-  /* Act on the event */
-  console.log(arr1[r.get()].name);
-  return false;
-});
 
 
 // console.log("the length of arr is: " + arr.length);
